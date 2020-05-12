e69bc4403361fcc8490ab620b946e753de133d4d
Andy <makimbell@gmail.com>
2020-05-11 22:39:18 -0400

    Added basic card UI

---
+++ javascript/javascript.js
@@ -0,0 +1,45 @@
+var wordList = getWordList()
+
+setUpcards(wordList);
+
+function getWordList () {
+  var wordList = [
+    ['one','two','three','four','five'],
+    ['six','seven','eight','9','10'],
+    ['11','12','13','14','15'],
+    ['16','17','18','19','20'],
+    ['21','22','23','24','25'],
+  ]
+  return wordList
+};
+
+function setUpcards(wordList) {
+
+  for(currentRow = 0; currentRow < 5; currentRow++){
+    for(currentCol = 0; currentCol < 5; currentCol++){
+
+      row = document.getElementById("row"+currentRow)
+      console.log(row);
+      
+      column = document.createElement("div")
+      column.className = "col-sm"
+  
+      card = document.createElement("div")
+      card.className = "card text-center"
+      card.id = currentRow.toString()+","+currentCol.toString()
+  
+      cardbody = document.createElement("div")
+      cardbody.className = "card-body"
+  
+      cardTitle = document.createElement("h2")
+      cardTitle.className = "card-title"
+      cardTitle.innerText = wordList[currentRow][currentCol];
+  
+      cardbody.appendChild(cardTitle);
+      card.appendChild(cardbody);
+      column.appendChild(card);
+      row.appendChild(column);
+
+    }
+  }
+}
